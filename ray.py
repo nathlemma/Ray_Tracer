1704bd4c290185ae8702a099552b07e9cbfe1b24
nathlemma <lemma.nath@icloud.com>
2023-02-16 17:28:21 -0500

    ray class

---
+++ ray.py
@@ -0,0 +1,25 @@
+class Rays(object):
+
+    def __init__(self, Os, Ds):
+        """
+        Initializes a bundle of rays containing the rays'
+        origins and directions.
+        """
+        self.Os = np.copy(Os)
+        self.Ds = np.copy(Ds)
+
+    def __call__(self, t):
+        """
+        Computes an array of 3D locations given the distances
+        to the points.
+        """
+        return self.Os + self.Ds * t[:, np.newaxis]
+
+    def __str__(self):
+        return "Os: " + str(self.Os) + "\n" + "Ds: " + str(self.Ds) + "\n"
+
+    def distance(self, point):
+        """
+        Compute the distances from the ray origins to a point
+        """
+        return np.linalg.norm(point[np.newaxis, :] - self.Os, axis=1)
