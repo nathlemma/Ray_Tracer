8da9f94c50ef1fb8c056a95b5d19a561e96193e5
nathlemma <lemma.nath@icloud.com>
2023-02-18 23:55:15 -0500

    ray modification

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-# Shade a scene given a bundle of eye rays; outputs a color image suitable for matplotlib visualization
-def shade(scene, rays):
-    shadow_ray_o_offset = 1e-6
-    L = L.reshape((scene.h, scene.w, 3))
-    return L
+#1. test montecarlo sampling 
+##     a.random
+##     b.cosine
+#2. test BRDF sampling
 
